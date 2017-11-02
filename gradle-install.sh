f56c5b5030676f44b7f6d7be8d8b8a06c27c1ed3
Ruben Trancoso <rubentrancoso@gmail.com>
2017-11-02 19:30:24 -0200

    gradle install / serverless-install

---
+++ gradle-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir gradle-files
+cd gradle-files
+# wget https://services.gradle.org/distributions/gradle-4.3-bin.zip
+sudo unzip -d /opt gradle-4.3-bin.zip
+sudo ln -s /opt/gradle-4.3 /opt/gradle
+
+sudo tee /etc/profile.d/gradle.sh <<-'EOF'
+export PATH=$PATH:/opt/gradle/bin
+EOF
+
+cd ..
+# rm -rf gradle-files
